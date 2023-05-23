0a2e83d0923c4ada4c88d8169dc9b8f5298a2ff6
andre <andres@abarrientoso.com>
2023-05-23 15:50:46 -0600

    cryptos

---
+++ src/data/monedas.js
@@ -0,0 +1,6 @@
+export const monedas = [
+    { id: 'USD', nombre: 'Dolar estadounidense'},
+    { id: 'CRC', nombre: 'Colon costarricense'},
+    { id: 'MXN', nombre: 'Peso Mexicano'},
+    { id: 'AUD', nombre: 'Dolar australiano'}
+]
